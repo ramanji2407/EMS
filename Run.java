6f6bd5b7f65e0fadf2d477399f90dd2caf6373fb
ram <pasupuletiramanji3@gmail.com>
2023-06-27 11:34:21 +0530

    commiting files

---
+++ Run.java
@@ -0,0 +1,21 @@
+import java.util.*;
+//
+//class Run{
+//    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println(sc.nextInt());
+//        sc.nextLine();
+//        System.out.println("Enter name");
+//        System.out.println(sc.nextLine());
+//
+//    }
+////}
+//  //  Comparator<Employ>E1=new Comparator<Employ>()
+////    {
+////        @Override
+////        public int compare(Employ o1, Employ o2) {
+////
+////                return o1.getName().compareTo(o2.getName()) ;
+////
+////        }
+////    }    ;
